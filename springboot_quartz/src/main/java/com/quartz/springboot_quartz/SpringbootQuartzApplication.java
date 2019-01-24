f009c5c85ebd9dee1f73b95194fd4f59222228bd
wzlove <2585700076@qq.com>
2019-01-24 14:11:22 +0800

    创建quartz项目

---
+++ springboot_quartz/src/main/java/com/quartz/springboot_quartz/SpringbootQuartzApplication.java
@@ -0,0 +1,14 @@
+package com.quartz.springboot_quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuartzApplication.class, args);
+    }
+
+}
+
